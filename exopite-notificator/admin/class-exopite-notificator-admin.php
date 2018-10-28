26fd01d75c81ffd7af9e728d6a668a4ed8b622ea
Joe <ujbeszel@gmail.com>
2018-10-28 12:29:54 +0100

    Remove '-meta' from meta key and add 'Exopite Client Detector' to handle client detection.

--- exopite-notificator/admin/class-exopite-notificator-admin.php
+++ exopite-notificator/admin/class-exopite-notificator-admin.php
@@ -110,7 +110,7 @@ class Exopite_Notificator_Admin {
 
             'type'              => 'menu',                          // Required, menu or metabox
             'id'                => $this->plugin_name,              // Required, meta box id, unique per page, to save: get_exopite_sof_option( id )
-            'menu'              => 'plugins.php',                   // Required, sub page to your options page
+            'parent'            => 'plugins.php',                   // Required, sub page to your options page
             'submenu'           => true,                            // Required for submenu
             'title'             => 'Exopite Notificator',            //The name of this page
             'capability'        => 'manage_options',                // The capability needed to view the page
@@ -127,11 +127,11 @@ class Exopite_Notificator_Admin {
               * METABOX
               */
              'type'              => 'metabox',                       // Required, menu or metabox
-             'id'                => $this->plugin_name . '-meta',    // Required, meta box id, unique, for saving meta: id[field-id]
+             'id'                => $this->plugin_name,              // Required, meta box id, unique, for saving meta: id[field-id]
              'post_types'        => array( 'post', 'page' ),         // Post types to display meta box
              'context'           => 'advanced',
              'priority'          => 'default',
-             'title'             => 'Exopite Notificator',                  // The name of this page
+             'title'             => 'Exopite Notificator',           // The name of this page
              'capability'        => 'edit_posts',                    // The capability needed to view the page
              // 'tabbed'            => false,
          );
@@ -631,167 +631,6 @@ class Exopite_Notificator_Admin {
 
     }
 
-    /*
-     * Get Browser data from HTTP_USER_AGENT
-     */
-    public function get_browser() {
-
-        $u_agent = $_SERVER['HTTP_USER_AGENT'];
-        $bname = $u_agent;
-        $platform = '';
-        $version= '';
-
-        //First get the platform?
-        if (preg_match('/linux/i', $u_agent)) {
-            $platform = 'Linux';
-        }
-        elseif (preg_match('/macintosh|mac os x/i', $u_agent)) {
-            $platform = 'Mac';
-        }
-        elseif (preg_match('/windows|win32/i', $u_agent)) {
-            $platform = 'Windows';
-        }
-
-        // Next get the name of the useragent yes seperately and for good reason
-        if(preg_match('/MSIE/i',$u_agent) && !preg_match('/Opera/i',$u_agent))
-        {
-            $bname = 'Internet Explorer';
-            $ub = "MSIE";
-        }
-        elseif(preg_match('/Firefox/i',$u_agent))
-        {
-            $bname = 'Mozilla Firefox';
-            $ub = "Firefox";
-        }
-        elseif(preg_match('/Chrome/i',$u_agent))
-        {
-            $bname = 'Google Chrome';
-            $ub = "Chrome";
-        }
-        elseif(preg_match('/Safari/i',$u_agent))
-        {
-            $bname = 'Apple Safari';
-            $ub = "Safari";
-        }
-        elseif(preg_match('/Opera/i',$u_agent))
-        {
-            $bname = 'Opera';
-            $ub = "Opera";
-        }
-        elseif(preg_match('/Netscape/i',$u_agent))
-        {
-            $bname = 'Netscape';
-            $ub = "Netscape";
-        }
-
-        // finally get the correct version number
-        $known = array('Version', $ub, 'other');
-        $pattern = '#(?<browser>' . join('|', $known) .
-        ')[/ ]+(?<version>[0-9.|a-zA-Z.]*)#';
-        if (!preg_match_all($pattern, $u_agent, $matches)) {
-            // we have no matching number just continue
-        }
-
-        // see how many we have
-        $i = count($matches['browser']);
-        if ($i != 1) {
-            //we will have two since we are not using 'other' argument yet
-            //see if version is before or after the name
-            if (strripos($u_agent,"Version") < strripos($u_agent,$ub)){
-                $version= $matches['version'][0];
-            }
-            else {
-                $version= $matches['version'][1];
-            }
-        }
-        else {
-            $version= $matches['version'][0];
-        }
-
-        // check if we have a number
-        if ($version==null || $version=="") {$version="?";}
-
-        return array(
-            'userAgent' => $u_agent,
-            'name'      => $bname,
-            'version'   => $version,
-            'platform'  => $platform,
-            'pattern'    => $pattern
-        );
-
-    }
-
-    /*
-     * Get user IP address
-     */
-    public function get_ip_address() {
-
-        // check for shared internet/ISP IP
-        if (!empty($_SERVER['HTTP_CLIENT_IP']) && $this->validate_ip($_SERVER['HTTP_CLIENT_IP'])) {
-            return $_SERVER['HTTP_CLIENT_IP'];
-        }
-
-        // check for IPs passing through proxies
-        if (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-            // check if multiple ips exist in var
-            if (strpos($_SERVER['HTTP_X_FORWARDED_FOR'], ',') !== false) {
-                $iplist = explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']);
-                foreach ($iplist as $ip) {
-                    if ($this->validate_ip($ip))
-                        return $ip;
-                }
-            } else {
-                if ($this->validate_ip($_SERVER['HTTP_X_FORWARDED_FOR']))
-                    return $_SERVER['HTTP_X_FORWARDED_FOR'];
-            }
-        }
-        if (!empty($_SERVER['HTTP_X_FORWARDED']) && $this->validate_ip($_SERVER['HTTP_X_FORWARDED']))
-            return $_SERVER['HTTP_X_FORWARDED'];
-        if (!empty($_SERVER['HTTP_X_CLUSTER_CLIENT_IP']) && $this->validate_ip($_SERVER['HTTP_X_CLUSTER_CLIENT_IP']))
-            return $_SERVER['HTTP_X_CLUSTER_CLIENT_IP'];
-        if (!empty($_SERVER['HTTP_FORWARDED_FOR']) && $this->validate_ip($_SERVER['HTTP_FORWARDED_FOR']))
-            return $_SERVER['HTTP_FORWARDED_FOR'];
-        if (!empty($_SERVER['HTTP_FORWARDED']) && $this->validate_ip($_SERVER['HTTP_FORWARDED']))
-            return $_SERVER['HTTP_FORWARDED'];
-
-        // return unreliable ip since all else failed
-        return $_SERVER['REMOTE_ADDR'];
-
-    }
-
-    /**
-     * Ensures an ip address is both a valid IP and does not fall within
-     * a private network range.
-     */
-    public function validate_ip($ip) {
-
-        if (strtolower($ip) === 'unknown')
-            return false;
-
-        // generate ipv4 network address
-        $ip = ip2long($ip);
-
-        // if the ip is set and not equivalent to 255.255.255.255
-        if ($ip !== false && $ip !== -1) {
-            // make sure to get unsigned long representation of ip
-            // due to discrepancies between 32 and 64 bit OSes and
-            // signed numbers (ints default to signed in PHP)
-            $ip = sprintf('%u', $ip);
-            // do private network range checking
-            if ($ip >= 0 && $ip <= 50331647) return false;
-            if ($ip >= 167772160 && $ip <= 184549375) return false;
-            if ($ip >= 2130706432 && $ip <= 2147483647) return false;
-            if ($ip >= 2851995648 && $ip <= 2852061183) return false;
-            if ($ip >= 2886729728 && $ip <= 2887778303) return false;
-            if ($ip >= 3221225984 && $ip <= 3221226239) return false;
-            if ($ip >= 3232235520 && $ip <= 3232301055) return false;
-            if ($ip >= 4294967040) return false;
-        }
-
-        return true;
-
-    }
-
     public function get_login_type() {
 
         if ( defined('XMLRPC_REQUEST') && XMLRPC_REQUEST ) {
@@ -1181,14 +1020,17 @@ class Exopite_Notificator_Admin {
     // Fields for all types
     public function get_template_fields() {
 
-        $user_browser = $this->get_browser();
+        $client = Exopite_Client_Detector::get_client();
+
+        // $user_browser = $this->get_browser();
 
         $template_fields = array(
-            'user-ip'       => $this->get_ip_address(),
+            'user-ip'       => $client['ip'],
             'datetime'      => date('Y-m-d H:i:s'),
             'site-url'      => get_site_url(),
             'site-name'     => get_bloginfo( 'name' ),
-            'user-agent'    => $user_browser['name'] . " | " . $user_browser['version'] . " | " . $user_browser['platform'],
+            'user-agent'    => $client['name'] . " | " . $client['version'] . " | " . $client['os'] . " | " . $client['platform'],
+            // 'user-agent'    => $user_browser['name'] . " | " . $user_browser['version'] . " | " . $user_browser['platform'],
         );
 
         if ( is_user_logged_in() ) {
@@ -1665,7 +1507,7 @@ class Exopite_Notificator_Admin {
         if ( ! isset( $options['metabox'] ) || $options['metabox'] == 'yes' ) {
 
             // Get post/page meta
-            $post_meta = get_post_meta( $comment->comment_post_ID, $this->plugin_name . '-meta' );
+            $post_meta = get_post_meta( $comment->comment_post_ID, $this->plugin_name );
 
             $item['email_smtp_override'] = $post_meta[0]['comment_email_smtp_override'];
             if ( $this->log ) $this->write_log( 'comment-new', 'postmeta: ' . var_export( $post_meta, true ) );
@@ -1734,7 +1576,7 @@ class Exopite_Notificator_Admin {
         if ( ! isset( $options['metabox'] ) || $options['metabox'] == 'yes' ) {
 
             // POST META
-            $post_meta = get_post_meta( $comment['comment_post_ID'], $this->plugin_name . '-meta' );
+            $post_meta = get_post_meta( $comment['comment_post_ID'], $this->plugin_name );
             $item['email_smtp_override'] = $post_meta[0]['comment_email_smtp_override'];
 
             if ( isset( $post_meta[0]['active_options'] ) && $post_meta[0]['active_options'] == 'no' ) {
@@ -1864,7 +1706,7 @@ class Exopite_Notificator_Admin {
 
         if ( ! isset( $options['metabox'] ) || $options['metabox'] == 'yes' ) {
 
-            $post_meta = get_post_meta( $post_id, $this->plugin_name . '-meta' );
+            $post_meta = get_post_meta( $post_id, $this->plugin_name );
             $item['email_smtp_override'] = $post_meta[0]['post_email_smtp_override'];
 
             if ( isset( $post_meta[0]['active_options'] ) && $post_meta[0]['active_options'] == 'no' ) {
