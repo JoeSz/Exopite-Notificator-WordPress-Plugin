e63ed3cdbea9780b88f3e14a558c4c87f055c79b
Joe <ujbeszel@gmail.com>
2019-03-03 15:33:49 +0100

    "continue" targeting switch is equivalent to "break".

--- exopite-notificator/vendors/telegram/notifcaster.class.php
+++ exopite-notificator/vendors/telegram/notifcaster.class.php
@@ -327,7 +327,7 @@ if ( ! class_exists( 'Notifcaster_Class' ) ) {
                     case false === $v = realpath(filter_var($v)):
                     case !is_file($v):
                     case !is_readable($v):
-                    continue; // or return false, throw new InvalidArgumentException
+                    continue 2; // or return false, throw new InvalidArgumentException
                 }
                 $data = file_get_contents($v);
                 $v = call_user_func("end", explode(DIRECTORY_SEPARATOR, $v));
